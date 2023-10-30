1fd3dc7d106adff644b2220c1e932770c5d47c52
gabicilfone <cilfonegabriel@gmail.com>
2023-10-30 11:12:12 -0300

    Add information to the result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,13 +1,25 @@
 import useQuoter from "../hooks/useQuoter"
+import { BRANDS,PLANS } from "../constants"
+
 const Result = () => {
 
-    const {result} = useQuoter()
+    const {result, datos} = useQuoter()
+    const { brand, plan, year } = datos
+
+    const [ nameBrand ] = BRANDS.filter(b => b.id === Number(brand))
 
     if (result === 0) return null
 
     return (
-        <div>
-        {result}
+        <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+            <h2 className="text-gray-600 font-black text-3xl">
+                Summary
+            </h2>
+
+            <p className="my-2">
+                <span className="font-bold">Brand: </span>
+                {nameBrand.name}
+            </p>
         </div>
     )
 }
